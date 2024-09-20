5e4e856e94a6bddf79e14ed8cba3e4038883c299
LeGrizzly <41363694+xalsie@users.noreply.github.com>
2024-09-20 07:50:00 +0200

    Uncomment jwt.js

--- api/src/middlewares/jwt.js
+++ api/src/middlewares/jwt.js
@@ -9,21 +9,18 @@ module.exports.generateToken = (user) => {
 
 // Vérification du token JWT
 module.exports.authenticateToken = (req, res, next) => {
-  req.user = "test";
-  next();
+  const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+  if (!token) {
+    logger.http(`${req.method} ${req.url} - ${res.statusCode}: Accès interdit, pas de token`);
+    return res.status(401).json({ message: 'Accès interdit : pas de token' });
+  }
 
-  // const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
-  // if (!token) {
-  //   logger.http(`${req.method} ${req.url} - ${res.statusCode}: Accès interdit, pas de token`);
-  //   return res.status(401).json({ message: 'Accès interdit : pas de token' });
-  // }
-
-  // jwt.verify(token, SECRET_KEY, (err, user) => {
-  //   if (err) {
-  //     logger.http(`${req.method} ${req.url} - ${res.statusCode}: Token invalide`);
-  //     return res.status(403).json({ message: 'Token invalide' });
-  //   }
-  //   req.user = user;
-  //   next();
-  // });
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      logger.http(`${req.method} ${req.url} - ${res.statusCode}: Token invalide`);
+      return res.status(403).json({ message: 'Token invalide' });
+    }
+    req.user = user;
+    next();
+  });
 };
