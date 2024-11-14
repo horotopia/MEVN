3f1fe0047bde509fd34dc89879b2138d81acac34
LeGrizzly <xalsie.ff@hotmail.fr>
2024-11-14 10:17:30 +0100

    fix(api): update user deletion logic to anonymize user data

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,31 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "../config/logger";
-import { User, IUserDocument } from '../models/user.model';
-import mongoose from 'mongoose';
+import { Address } from "../models/address.model";
+import { User } from '../models/user.model';
 
-const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+const getUsers = async (res: Response) => {
   try {
     const result = await User.find();
 
-    res.status(200).setHeader("Content-Type", "application/json").json(result);
+    res.status(200).json(result);
   } catch (err: Error | any) {
     logger.error(`Error retrieving users: ${err}`);
     res.status(500).json({ message: err.message });
   }
 };
 
-const postUser = async (req: Request, res: Response, next: NextFunction) => {
+const postUser = async (req: Request, res: Response) => {
   try {
     const result = await User.create(req.body);
 
-    res.status(201).setHeader("Content-Type", "application/json").json(result);
+    res.status(201).json(result);
   } catch (err: Error | any) {
     logger.error(`Error displaying user: ${err}`);
     res.status(500).json({ message: err.message });
   }
 };
 
-const getUser = async (req: Request, res: Response, next: NextFunction) => {
+const getUser = async (req: Request, res: Response) => {
   try {
     const result = await User.findById(req.params.id);
     if (!result) {
@@ -33,14 +33,14 @@ const getUser = async (req: Request, res: Response, next: NextFunction) => {
       throw new Error("User not found.");
     }
 
-    res.status(200).setHeader("Content-Type", "application/json").json(result);
+    res.status(200).json(result);
   } catch (err: Error | any) {
     logger.error(`Error retrieving user: ${err}`);
     res.status(500).json({ message: err.message });
   }
 };
 
-const putUser = async (req: Request, res: Response, next: NextFunction) => {
+const putUser = async (req: Request, res: Response) => {
   try {
     const result = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -51,14 +51,14 @@ const putUser = async (req: Request, res: Response, next: NextFunction) => {
       throw new Error("User not found.");
     }
 
-    res.status(200).setHeader("Content-Type", "application/json").json(result);
+    res.status(200).json(result);
   } catch (err: Error | any) {
     logger.error(`Error updating user: ${err}`);
     res.status(500).json({ message: err.message });
   }
 };
 
-const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+const deleteUser = async (req: Request, res: Response) => {
   const userId = req.params.id;
 
   try {
@@ -68,12 +68,28 @@ const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
       throw new Error("User not found");
     }
 
-    await user.deleteOne({ _id: userId });
+    user.name = "deleted";
+    user.email = userId + "@deleted.com";
+    user.password = "deleted";
+    user.save();
+
+    const address = await Address.find({ userId: userId });
+
+    if (address) {
+      address.forEach(async (address) => {
+        address.street = "deleted";
+        address.city = "deleted";
+        address.postalCode = "deleted";
+        address.country = "deleted";
+        await address.save();
+      });
+    }
 
     res.status(204).json({ success: true, message: "User deleted successfully" });
   } catch (err: Error | any) {
     logger.error(`Error deleting user: ${err}`);
     res.status(500).json({ message: err.message });
-  }};
+  }
+};
 
 export { deleteUser, getUser, getUsers, postUser, putUser };
