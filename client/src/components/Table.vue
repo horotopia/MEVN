7f2bb1c23237d5cf787edc2a878c90602571efcd
LeGrizzly <xalsie.ff@hotmail.fr>
2025-01-07 05:08:48 +0100

    Add export functionality to Table component and improve search input layout

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -15,7 +15,8 @@ const props = defineProps({
 				title: 'Table list',
 				description: 'Description'
 			},
-			search: false
+			search: false,
+            export: true
 		})
 	},
 	fields: {
@@ -89,7 +90,7 @@ const prevPage = function() {
 }
 
 const nextPage = function() {
-	if (currentPage.value < totalPages) {
+	if (currentPage.value < totalPages.value) {
 		currentPage.value++;
 	}
 }
@@ -154,30 +155,32 @@ watch(search, () => {
                 <h3 class="text-lg font-semibold text-slate-800">{{ props.options.header.title }}</h3>
                 <p class="text-slate-500">{{ props.options.header.description }}</p>
             </div>
-            <div class="ml-3" v-if="props.options.search">
-                <div class="w-full max-w-sm min-w-[200px] relative">
-                    <div class="relative">
-                        <input
-                            class="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
-                            placeholder="Rechercher dans la liste" type="text" id="search" name="search" v-model="search" />
-                        <button class="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
-                            type="button">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
-                                stroke="currentColor" class="w-8 h-8 text-slate-600">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                            </svg>
-                        </button>
+            <div class="flex items-center">
+                <div class="ml-3" v-if="props.options.export">
+                    <div class="w-full max-w-sm relative">
+                        <div class="relative">
+                            <button class="my-auto px-2 flex items-center bg-white rounded "
+                                type="button" @click="exportCsv">
+                                Exporter en csv
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="ml-3" v-if="props.options.search">
-                <div class="w-full max-w-sm min-w-[200px] relative">
-                    <div class="relative">
-                        <button class="my-auto px-2 flex items-center bg-white rounded "
-                            type="button" @click="exportCsv">
-                            Exporter en csv
-                        </button>
+                <div class="ml-3" v-if="props.options.search">
+                    <div class="w-full max-w-sm relative">
+                        <div class="relative">
+                            <input
+                                class="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+                                placeholder="Rechercher dans la liste" type="text" id="search" name="search" v-model="search" />
+                            <button class="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
+                                type="button">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                                    stroke="currentColor" class="w-8 h-8 text-slate-600">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
