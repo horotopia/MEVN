b19c39519b35fd474b251ab92b8e92322bb22f18
Arthur serenne <arthur.serenne@gmail.com>
2025-01-08 22:37:42 +0100

    ConsoleLog error

--- api/src/tests/integration/orders.controller.test.ts
+++ api/src/tests/integration/orders.controller.test.ts
@@ -190,7 +190,7 @@ describe('Bon bah on va tester les routes des commandes', () => {
         app.use('/api/orders', ordersController.buildRouter());
 
         app.use((err: any, req: any, res: any, next: any) => {
-            console.error('Error:', err);
+            // console.error('Error:', err);
             res.status(res.statusCode || 500).json({ message: err.message });
         });
 
